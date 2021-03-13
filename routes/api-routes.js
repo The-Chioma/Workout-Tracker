0807bd4c6dfd952287e5705d3eaa2d22c293ae3b
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-03-13 17:59:03 +0000

    created api routes and connection to server

---
+++ routes/api-routes.js
@@ -0,0 +1,56 @@
+const Workout = require("../models/workout");
+
+module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workout", function (req, res) {
+    Workout.create({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workout/range", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workout/range", function (req, res) {
+    Workout.create({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workout/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    ).then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+  });
+};
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
